419a0b72b59b16d0b183136ca9c4a9fdf2e2d3b2
bestchse <48196097+bestchse@users.noreply.github.com>
2022-10-20 17:03:11 +0700

    add some music

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,31 +1,13 @@
 import React, { Component } from 'react'
 import YouTube from 'react-youtube';
+import Playlist from '../datajson/playlist.json'
 
 export class Test extends Component {
     constructor() {
         super();
         this.state = {
-            Data: [
-                {
-                    Key: 1,
-                    VideoId: "Xlyi_8Z2tFU",
-                    VideoName: "ร้องเพลงฟังสบายชิลๆ ผ่อนคลายไปพร้อมกัน",
-                    MusicName: "Please",
-                    MusicTime: 4838,
-                    Band: "Atom ชนกันต์",
-                    FullUrl: "https://www.youtube.com/watch?v=Xlyi_8Z2tFU&t=4838s",
-                },
-                {
-                    Key: 2,
-                    VideoId: "Xlyi_8Z2tFU",
-                    VideoName: "ร้องเพลงฟังสบายชิลๆ ผ่อนคลายไปพร้อมกัน",
-                    MusicName: "ธรรมดาที่แสนพิเศษ",
-                    MusicTime: 5537,
-                    Band: "ANATOMY RABBIT",
-                    FullUrl: "https://youtu.be/Xlyi_8Z2tFU?t=5537",
-                },
-            ],
-            Play: {
+            Playlist: Playlist.Data,
+            Player: {
                 VideoId: null,
                 Options: {
                     height: '100',
@@ -36,18 +18,31 @@ export class Test extends Component {
                         controls: 0
                     }
                 }
-            }
+            },
+            SearchValue: "",
         };
     }
-    play(Key) {
+
+    ConvertTime(Time) {
+        let hms = Time;
+        let separate = hms.split(':');
+        let seconds = (+separate[0]) * 60 * 60 + (+separate[1]) * 60 + (+separate[2]);
+        return seconds
+    }
+
+    PlayMusic(Key) {
+        let MusicStart = this.ConvertTime(this.state.Playlist[Key - 1].MusicTimeStart)
+        let MusicEnd = this.ConvertTime(this.state.Playlist[Key - 1].MusicTimeEnd)
+
         this.setState({
-            Play: {
-                VideoId: this.state.Data[Key - 1].VideoId,
+            Player: {
+                VideoId: this.state.Playlist[Key - 1].VideoId,
                 Options: {
                     height: '100',
                     width: '200',
                     playerVars: {
-                        start: this.state.Data[Key - 1].MusicTime,
+                        start: MusicStart,
+                        end: MusicEnd,
                         autoplay: 1,
                         controls: 0
                     }
@@ -56,17 +51,26 @@ export class Test extends Component {
         })
     }
 
+    Search = (event) => {
+        this.setState({
+            SearchValue: event.target.value
+        })
+    }
+
     render() {
-        console.log(this.state.Play)
         return (
             <div>
-                <YouTube videoId={this.state.Play.VideoId} opts={this.state.Play.Options} />
+                <YouTube videoId={this.state.Player.VideoId} opts={this.state.Player.Options} />
+                <div>
+                    <p>{this.state.SearchValue}</p>
+                    <p>
+                        <input placeholder='Search' value={this.state.SearchValue} onChange={this.Search}></input>
+                    </p>
+                </div>
                 {
-                    this.state.Data.map(data =>
+                    this.state.Playlist.map(data =>
                         <div key={data.Key}>
-                            {/* <p>{data.VideoName}</p> */}
-                            <a onClick={() => this.play(data.Key)}>{data.Band} - {data.MusicName}</a>
-                            {/* <YouTube videoId={data.VideoId} opts={data.YoutubePlayer} /> */}
+                            <a onClick={() => this.PlayMusic(data.Key)}>{data.Band} - {data.MusicName}</a>
                         </div>
                     )
                 }
